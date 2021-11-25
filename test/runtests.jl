00da9200800f468caf775791ae8261e6576d830e
Dimitar Vanguelov <dvanguelov@gmail.com>
2021-11-25 10:23:00 -0500

    Add very basic test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,14 @@
 using TerminalCalendar
 using Test
+using Dates
 
 @testset "TerminalCalendar.jl" begin
-    # Write your tests here.
+    feb2021 = Date(2021, 2)
+
+    feb2021_auto = TerminalCalendar.calendarize(feb2021)
+    # feb2021_manual = Matrix{Union{Missing, Date}}()
+    feb_range = Date(2021, 2, 1):Day(1):Date(2021, 2, 28)
+    feb_matrix = permutedims(reshape(feb_range, 7, :))
+
+    @test feb2021_auto == feb_matrix
 end
