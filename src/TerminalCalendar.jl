4b4c48058ae1fa3207ec36021d86f34cb9c5cef1
Dimitar Vanguelov <dvanguelov@gmail.com>
2021-11-25 10:05:48 -0500

    Add very basic docstring to `calendar` function.

--- src/TerminalCalendar.jl
+++ src/TerminalCalendar.jl
@@ -40,6 +40,53 @@ end
 
 ### exported functions ###
 
+"""
+    calendar(dt::Date)
+
+Pretty prints the monthly calendar of `dt`.
+
+# Example
+
+```jldoctest
+julia> calendar(Date(2021, 12))
+
+              December 2021
+┌─────┬─────┬─────┬─────┬─────┬─────┬─────┐
+│ Mon │ Tue │ Wed │ Thu │ Fri │ Sat │ Sun │
+├─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+│     │     │   1 │   2 │   3 │   4 │   5 │
+│   6 │   7 │   8 │   9 │  10 │  11 │  12 │
+│  13 │  14 │  15 │  16 │  17 │  18 │  19 │
+│  20 │  21 │  22 │  23 │  24 │  25 │  26 │
+│  27 │  28 │  29 │  30 │  31 │     │     │
+└─────┴─────┴─────┴─────┴─────┴─────┴─────┘
+```
+
+    calendar(yr::Int, mo::Int)
+
+Pretty prints the monthly calendar for `yr` and `mo`.
+
+# Example
+
+```jldoctest
+julia> calendar(2021, 12)
+
+              December 2021
+┌─────┬─────┬─────┬─────┬─────┬─────┬─────┐
+│ Mon │ Tue │ Wed │ Thu │ Fri │ Sat │ Sun │
+├─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+│     │     │   1 │   2 │   3 │   4 │   5 │
+│   6 │   7 │   8 │   9 │  10 │  11 │  12 │
+│  13 │  14 │  15 │  16 │  17 │  18 │  19 │
+│  20 │  21 │  22 │  23 │  24 │  25 │  26 │
+│  27 │  28 │  29 │  30 │  31 │     │     │
+└─────┴─────┴─────┴─────┴─────┴─────┴─────┘
+```
+
+And there is more! Input different things and see what works!
+"""
+function calendar end
+
 # date type arguments
 function calendar(dt::Date = today(); tf=tf_unicode)
     calenderized_dt = calendarize(dt)
