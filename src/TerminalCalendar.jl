9725325e00eb5048e89159394199ce7b07012a18
Dimitar Vanguelov <dvanguelov@gmail.com>
2021-11-25 13:50:13 -0500

    Add precompile statements

--- src/TerminalCalendar.jl
+++ src/TerminalCalendar.jl
@@ -162,6 +162,33 @@ function calendar(
     end
 end
 
+precompile(calendar, (Date, ))
+
+precompile(calendar, (Vector{Date}, ))
+precompile(calendar, (StepRange{Date, Month}, ))
+
+precompile(calendar, (Int, ))
+precompile(calendar, (Int, Int))
+
+precompile(calendar, (Vector{Int64}, Int))
+precompile(calendar, (UnitRange{Int64}, Int))
+precompile(calendar, (StepRange{Int64}, Int))
+
+precompile(calendar, (Int, Vector{Int64}))
+precompile(calendar, (Int, UnitRange{Int64}))
+precompile(calendar, (Int, StepRange{Int64}))
+
+precompile(calendar, (Vector{Int64}, Vector{Int64}))
+precompile(calendar, (Vector{Int64}, UnitRange{Int64}))
+precompile(calendar, (Vector{Int64}, StepRange{Int64}))
+
+precompile(calendar, (UnitRange{Int64}, Vector{Int64}))
+precompile(calendar, (UnitRange{Int64}, UnitRange{Int64}))
+precompile(calendar, (UnitRange{Int64}, StepRange{Int64}))
+
+precompile(calendar, (StepRange{Int64}, Vector{Int64}))
+precompile(calendar, (StepRange{Int64}, UnitRange{Int64}))
+precompile(calendar, (StepRange{Int64}, StepRange{Int64}))
 
 export calendar
 
