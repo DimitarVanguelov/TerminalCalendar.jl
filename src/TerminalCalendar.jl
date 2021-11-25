5cc84ae749c949df0c22dc3b589747fc9b160d6f
Dimitar Vanguelov <dvanguelov@gmail.com>
2021-11-24 21:53:12 -0500

    Add additional methods to `calendar`

--- src/TerminalCalendar.jl
+++ src/TerminalCalendar.jl
@@ -3,6 +3,7 @@ module TerminalCalendar
 using Dates
 using PrettyTables
 
+
 # turn month of `dt` into 5x7 array of dates with missing values
 function calendarize(dt::Date)
     first_day = firstdayofmonth(dt)
@@ -17,9 +18,12 @@ function calendarize(dt::Date)
     return permutedims(reshape(padded_dates, 7, :))
 end
 
+
 # formatter function to replace missings, thanks to @ronisbr
 ft_missing = (v, i, j) -> ismissing(v) ? "" : v
 
+
+# date type arguments
 function calendar(dt::Date = today())
     calenderized_dt = calendarize(dt)
     mn = monthname(dt)
@@ -32,10 +36,21 @@ function calendar(dt::Date = today())
     )
 end
 
-function calendar(
-    year::Integer,
-    month::Integer
-    )
+function calendar(dates::Vector{Date})
+    for dt in dts
+        calendar(dt)
+    end
+end
+
+function calendar(dts::StepRange{Date, Month})
+    for dt in dts
+        calendar(dt)
+    end
+end
+
+
+# integer type arguments
+function calendar(year::Integer, month::Integer)
     dt = Date(year, month)
     calendar(dt)
 end
@@ -45,6 +60,67 @@ function calendar(year::Integer)
         calendar(Date(year, month))
     end
 end
+
+function calendar(years::Vector{Int64})
+    for year in years
+        calendar(year)
+    end
+end
+
+function calendar(years::UnitRange{Int64})
+    for year in years
+        calendar(year)
+    end
+end
+
+function calendar(years::Vector{Int64}, month::Integer)
+    for year in years
+        calendar(year, month)
+    end
+end
+
+function calendar(years::UnitRange{Int64}, month::Integer)
+    for year in years
+        calendar(year, month)
+    end
+end
+
+function calendar(year::Integer, months::Vector{Int64})
+    for month in months
+        calendar(year, month)
+    end
+end
+
+function calendar(year::Integer, months::UnitRange{Int64})
+    for month in months
+        calendar(year, month)
+    end
+end
+
+function calendar(years::Vector{Int64}, months::Vector{Int64})
+    for year in years, month in months
+        calendar(year, month)
+    end
+end
+
+function calendar(years::UnitRange{Int64}, months::UnitRange{Int64})
+    for year in years, month in months
+        calendar(year, month)
+    end
+end
+
+function calendar(years::Vector{Int64}, months::UnitRange{Int64})
+    for year in years, month in months
+        calendar(year, month)
+    end
+end
+
+function calendar(years::UnitRange{Int64}, months::Vector{Int64})
+    for year in years, month in months
+        calendar(year, month)
+    end
+end
+
 
 export calendar
 
