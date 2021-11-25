c8542f8aa7d5026f290c7a6d870891d5d6811f0a
Dimitar Vanguelov <dvanguelov@gmail.com>
2021-11-25 00:21:24 -0500

    Center month-year header

--- src/TerminalCalendar.jl
+++ src/TerminalCalendar.jl
@@ -4,7 +4,9 @@ using Dates
 using PrettyTables
 
 
-# turn month of `dt` into 5x7 array of dates with missing values
+### helper functions ###
+
+# turn month of `dt` into nx7 array of dates with missing values
 function calendarize(dt::Date)
     first_day = firstdayofmonth(dt)
     last_day = lastdayofmonth(dt)
@@ -23,17 +25,36 @@ end
 ft_missing = (v, i, j) -> ismissing(v) ? "" : v
 
 
+# center month-year header for each monthly calendar
+function create_month_year_header(dt::Date, table_str::AbstractString)
+    mo = monthname(dt)
+    yr = year(dt)
+    byte_index = findfirst('\n', table_str)
+    table_width = length(table_str, 1, byte_index)
+    month_year_width = length(mo) + length(string(yr)) + 1
+    front_pad_width = Int(floor((table_width - month_year_width) / 2)) - 1
+    front_pad = repeat(' ', front_pad_width)
+    return string('\n', front_pad, "$mo $yr", '\n')
+end
+
+
+### exported functions ###
+
 # date type arguments
 function calendar(dt::Date = today())
     calenderized_dt = calendarize(dt)
-    mn = monthname(dt)
-    yr = year(dt)
-    printstyled("\n               $mn $yr \n", bold=true)
-    pretty_table(
+
+    table_str = pretty_table(
+        String,
         day.(calenderized_dt),
         header=first.(dayname.(1:7), 3),
-        formatters=ft_missing
+        formatters=ft_missing,
     )
+
+    month_year_header = create_month_year_header(dt, table_str)
+
+    printstyled(month_year_header, bold=true)
+    print(table_str)
 end
 
 function calendar(dates::Vector{Date})
